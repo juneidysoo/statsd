1a01222b21cccbd1405b3f21e7c6d6027a4d5d18
Juneidy <j@awesomest.me>
2018-11-01 00:07:15 +0800

    Added tcp.

--- test/server.js
+++ test/server.js
@@ -1,16 +1,25 @@
 const dgram = require('dgram');
+const fs = require('fs');
 const assert = require('assert');
-const server = require('../src/server');
+const net = require('net');
+const udp = require('../src/server/udp');
+const tcp = require('../src/server/tcp');
 
 const msg = 'This is a test\r\n';
 
 describe(__filename, () => {
+	const createConfig = config => ({ get: key => config[key] });
+
+	const c = {
+		port: 8125,
+		address: '127.0.0.1'
+	};
+
 	it('udp', done => {
-		const s = server.start((data, rinfo) => {
+		const s = udp.start(createConfig(c), null, (data, rinfo) => {
 			assert.equal(msg, data.toString());
 			assert.equal(msg.length, rinfo.size);
-			s.close();
-			done();
+			s.close(done);
 		});
 
 		const sock = dgram.createSocket('udp4');
@@ -23,5 +32,52 @@ describe(__filename, () => {
 			'127.0.0.1',
 			() => sock.close()
 		);
+	});
+
+	describe('tcp', () => {
+		it('data received', done => {
+			const s = tcp.start(createConfig(c), null, (data, rinfo) => {
+				assert.equal(msg, data.toString());
+				assert.equal(msg.length, rinfo.size);
+				s.close(done);
+			});
+
+			const client = net.connect(c.port, c.address, () => {
+				client.write(msg);
+				client.end();
+			});
+		});
+
+		it('data buffered', done => {
+			const s = tcp.start(createConfig(c), null, (data, rinfo) => {
+				assert.equal(msg, data.toString());
+				assert.equal(msg.length, rinfo.size);
+				s.close(done);
+			});
+
+			const client = net.connect(c.port, c.address, () => {
+				client.setNoDelay(true);
+				msg.split(' ').forEach(part => {
+					client.write(part);
+					client.write(' ');
+				});
+				client.end();
+			});
+		});
+
+		it('socket', done => {
+			const conf = Object.assign({ socket: '/tmp/statsd.socket' }, c);
+			const s = tcp.start(createConfig(conf), null, (data, rinfo) => {
+				assert.equal(msg, data.toString());
+				assert.equal(msg.length, rinfo.size);
+				fs.unlinkSync(conf.socket);
+				s.close(done);
+			});
+
+			var client = net.connect(conf.socket, function () {
+				client.write(msg);
+				client.end();
+			});
+		});
 	});
 });
